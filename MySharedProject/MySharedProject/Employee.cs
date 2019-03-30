a216a845b75ac9d5ce1302d7d1895eb2015aabf9
Rod Falanga <rfalanga@users.noreply.github.com>
2019-03-30 08:09:41 -0600

    Initial commit of the Shared Project portion of MySharedProject.

---
+++ MySharedProject/MySharedProject/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySharedProject
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        //Methods
+        public string GetEmployeeDetail()
+        {
+            EmployeeID = 247;
+            FirstName = "Ved";
+            LastName = "Prakasj";
+            Phone = "5055551234";
+            Address = "Albuquerque, New Mexico";
+
+            return string.Concat($"Name: {FirstName} {LastName}\nEmp ID: {EmployeeID}\nAddress: {Address}\nPhone: {Phone}");
+        }
+    }
+}
